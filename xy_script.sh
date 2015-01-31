cc172fc3c62e12fc899ed375c29c481174e4bb4a
Spencer Wright <snwright@gmail.com>
2015-01-30 20:16:15 -0500

    updated script per failure at WAi
    added note in comments that the partlist must be exported from layout
    editor, NOT schematic editor.
    
    changed tail command to not cut off the first line of the partlist :/

--- xy_script.sh
+++ xy_script.sh
@@ -10,6 +10,10 @@
 #
 # This shell script takes a plain text file (the result of exporting a partlist from EAGLE CAD) as argument
 # and creates a new CSV file formatted with part names, x positions, and y positions.
+#
+# NOTE: PARTLIST MUST BE GENERATED FROM THE LAYOUT EDITOR! Partlists exported from the schematic editor do NOT
+# contain x/y data and will not work with this script.
+#
 # usage: go to the directory where this file is stored, then type:
 # 		./xy_script.sh [filename]
 # And the script will generate a file named [filename_xy] that contains:
@@ -23,10 +27,10 @@
 partlist="$1"						#sets variable from stdin
 xy=$1_xy							#sets filename for output
 
-touch $1_xy							#creates output file
+touch $xy							#creates output file
 echo Part\ name,xpos,ypos, > $xy		#writes column names to first line of output
 
-tail -n +12 $partlist |				#skips the first 11 lines of the file, which contain eagle output
+tail -n +11 $partlist |				#skips the first 10 lines of the file, which contain eagle output
 awk 'match($0, /\([^)]+\)/) {print $1,substr($0, RSTART, RLENGTH)}' |	#parses each line and returns the first word (part namesme), plus the entire (x,y) coordinate
 sed -e 's/ (/,/' |					#removes the space after the part name and the first paren and replaces them with a comma
 sed -e 's/ /,/' |					#removes the space between xpos and ypos and replaces it with a comma
