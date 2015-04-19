d596d08d3ca5ba6cedc62ac5c68893156ecb5bfd
Suz Hinton <suz.hinton@gmail.com>
2015-04-19 00:05:41 -0400

    bundle files and run scripts

--- MakrKT/Maker.py
+++ MakrKT/Maker.py
@@ -4,55 +4,57 @@ from Tkinter import *
 import subprocess
 import tempfile
 import os
+import sys
 Popen = subprocess.Popen
 PIPE = subprocess.PIPE
+
+def resource_path(relative_path):
+  if getattr(sys, 'frozen', False):
+    if hasattr(sys, "_MEIPASS"):
+      # we are running in a |PyInstaller| bundle
+      basedir = sys._MEIPASS
+    elif os.environ.get('_MEIPASS2'):
+      # we are running in a |PyInstaller| bundle
+      basedir = os.environ.get('_MEIPASS2')
+  else:
+    # we are running in a normal Python environment
+    basedir = os.path.dirname(__file__)
+
+  # return 'real' path to file
+  return os.path.join(basedir, relative_path)
 
 # get current directory we're in
 current_dir = os.getcwd()
 # get os path of our scripts we wanna use later
-eeprom_script_path = os.path.join(current_dir, 'eeprom.py')
-pr_hex_path = os.path.join(current_dir, 'pr.hex')
-hero_pic_path = os.path.join(current_dir, 'pic.gif')
+eeprom_script_path = resource_path('eeprom.py')
+pr_hex_path = resource_path('pr.hex')
+hero_pic_path = resource_path(os.path.join('.', 'pic.gif'))
 
 # method to run eeprom.py and write to a temporary file
 def build_hex():
   # set tempfile to not delete upon close() because we gotta close it to pass it in to avrdude
   make_tempfile = tempfile.NamedTemporaryFile(prefix='eeprom.', suffix='.hex', delete=False)
-
   # run eeprom.py using the entered frequency of choice
-  make_eeprom = Popen([eeprom_script_path, '-f %s' % e1.get()], stdout=PIPE)
-  # read the output of the script
-  eeprom_output = make_eeprom.stdout.read()
-  # wait just in case
-  make_eeprom.wait()
+  make_eeprom = subprocess.check_output(eeprom_script_path + ' -f %s' % e1.get(), shell=True)
   # pipe the output of eeprom.py into the tempfile
-  make_tempfile.write(eeprom_output)
+  make_tempfile.write(make_eeprom)
+  # for debug
+  log_string.set(make_eeprom)
   # we're done with the file
   make_tempfile.close()
-
-  #print 'temp file: ' + make_tempfile.name
   flash_hex(make_tempfile)
 
 # run avrdude with prior built temporary hex file
 def flash_hex(make_tempfile):
   # chain together the avr dude command with flags
   avrdude_cmd = 'avrdude -q -P usb -c %s -p attiny45 -b 15 -e -U flash:w:%s -U eeprom:w:%s' % (e2.get(), pr_hex_path, make_tempfile.name)
+  print avrdude_cmd
   # open subprocess to run avrdude
-  avrdude = Popen(avrdude_cmd.split(), stderr=PIPE)
-  # pipe avrdude stderr because they doing something wacky instead of using stdout
-  avrdude_err = avrdude.stderr.read()
-
-  #print avrdude_cmd
+  avrdude = Popen(avrdude_cmd, stderr=PIPE, shell=True)
+  avrdude_err = avrdude.communicate()[1]
   
-  # gah this error check will never return 0 as avrdude just writes stdout to stderr instead *grumpy face*
-  # get rid of this all together and maybe grep for the right success phrase which is totally not fragile at all B-)
-  # I wonder if stdout is actually treated like stderr maybe test this
   # log_string is the var that is bound to the status label at the bottom of the GUI
-  if (avrdude_err != 0):
-    log_string.set('Oh no! There was an error:\n\n' + avrdude_err)
-  else:
-    log_string.set('Yay!\nYour radio was successfully reprogrammed!')
-
+  log_string.set(avrdude_err)
   # cool so we can delete this file now
   os.remove(make_tempfile.name)
 
@@ -88,7 +90,7 @@ e1.insert(10, '97.1')
 e2.insert(10, 'usbtiny')
 
 # Flash button setup, runs the build_hex method on click
-Button(master, text='Flash', command=build_hex, relief=RAISED).grid(row=3, column=2, sticky=E+N, padx=10, pady=10)
+Button(master, text='Flash', command=build_hex).grid(row=3, column=2, sticky=E+N, padx=10, pady=10)
 #Button(master, text='Quit', command=master.quit).grid(row=3, column=2, sticky=W, pady=4)
 
 # run TK loop
